3f49cf32fcdbbc0ad66c810f74eeaffe56cabbcc
guannan15 <guannan15@staff.weibo.com>
2025-06-29 11:17:55 +0800

    添加视频Boom_Test.mp4和两个缩放脚本

---
+++ zoom_script.sh
@@ -0,0 +1,18 @@
+ffmpeg -i "Boom_Test.mp4" -filter_complex "
+[0:v]split=3[pre][zoom][post];
+
+[zoom]trim=start=3:end=11,setpts=PTS-STARTPTS,
+zoompan=
+  z='if(lt(it,2), 1+it/2,
+     if(lt(it,6), 2,
+     if(lt(it,8), 2 - (it-6)/2, 1)))':
+  x='iw/2-(iw/zoom/2)':
+  y='ih/2-(ih/zoom/2)':
+  d=1:fps=30:s=1366x768
+[zoomed];
+
+[pre]trim=end=3,setpts=PTS-STARTPTS[first];
+[post]trim=start=11,setpts=PTS-STARTPTS[last];
+
+[first][zoomed][last]concat=n=3:v=1:a=0[outv]
+" -map "[outv]" -map 0:a -c:v libx264 -c:a copy -y Boom_Test_zoom_fixed.mp4
